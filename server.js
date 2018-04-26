71a1f05066d0ecd248f35048063e93cc9569d70b
gabriel <gabrielmatheusvieira@gmail.com>
2018-04-26 20:52:06 -0300

    26-04

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Product = require('./app/models/product');
+
+mongoose.connect('mongodb://gabrielmatheus:G@br1el1@ds014368.mlab.com:14368/superbanco')
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -9,9 +13,32 @@ var port = process.env.port || 8000;
 
 var router = express.Router();
 
+//router.use(function(req, res, next) {
+//	console.log("interceptacao pelo middleware ok");
+//	next();
+//});
+
 router.get('/', function (req, res) {
 	res.json({'message':'Ok, rota principal funcionando'});
 });
+
+
+router.route('/produtos')
+ 
+  .post(function(req, res) {
+  	var product = new Product();
+  	product.name = req.body.name;	
+  	product.price = req.body.price;
+  	product.description = req.body.description;
+  
+  	product.save(function(err) {
+  		if(error)
+  			res.send("Erro ao salvar produto no banco" + error);
+  		
+  		res.json({message:"Produto inserido com sucesso."});
+  	});
+
+  });
 
 app.use('/api', router);
 
